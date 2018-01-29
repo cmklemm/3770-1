93983832d650d8717f7f758ea9037652091d7741
cklemm <cklemm@DESKTOP-31R7MEV.vdps.net>
2018-01-29 16:16:34 -0500

    Beer song class added, functions Main() and Ninety_Nine_Bottles_of_Beer() added to class, empty.

---
+++ src/csi3770/BeerSong.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csi3770;
+
+/**
+ *
+ * @author cklemm
+ */
+public class BeerSong {
+    
+    public void Main(){
+        
+    }
+    
+    public void Ninety_Nine_Bottles_of_Beer() {
+        
+    }
+}
