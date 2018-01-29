002bdf0bec4322b07964aa7e89d4165eee4f752b
cklemm <cklemm@DESKTOP-31R7MEV.vdps.net>
2018-01-29 16:32:41 -0500

    Added functionality to Ninety_Nine_Bottles_of_Beer(). Added calls to that function in the main function of both BeerSong and the main class for the package.
    The function runs off a loop, except for the final two verses which are handled as standalone verses to keep subject-verb agreement.

--- src/csi3770/BeerSong.java
+++ src/csi3770/BeerSong.java
@@ -11,11 +11,20 @@ package csi3770;
  */
 public class BeerSong {
     
-    public void Main(){
-        
+    public static void main(String[] args){
+        Ninety_Nine_Bottles_of_Beer();
     }
     
-    public void Ninety_Nine_Bottles_of_Beer() {
+    public static void Ninety_Nine_Bottles_of_Beer() {
+        for (int i=99; i > 2; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer\n" +
+                "Take one down, pass it around, " + (i-1) + " bottles of beer on the wall.");
+        }
         
+        System.out.println("2 bottles of beer on the wall, 2 bottles of beer\n" +
+            "Take one down, pass it around, 1 bottle of beer on the wall.");
+        
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer\n" +
+            "Take one down, pass it around, no more bottles of beer on the wall.");
     }
 }
